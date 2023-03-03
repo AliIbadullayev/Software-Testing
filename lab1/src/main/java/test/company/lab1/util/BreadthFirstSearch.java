102fe21fa28b65a6082bbf82cf7de250c2c409af
lwbeamer <olibabamimedovich@gmail.com>
2023-03-03 20:51:33 +0300

    2nd task done

--- lab1/src/main/java/test/company/lab1/util/BreadthFirstSearch.java
+++ lab1/src/main/java/test/company/lab1/util/BreadthFirstSearch.java
@@ -5,69 +5,54 @@ import java.util.LinkedList;
 
 public class BreadthFirstSearch {
 
-    private int V; // No. of vertices
-    private LinkedList<Integer>[] adj; // Adjacency Lists
+    private int V;
+    private LinkedList<Integer>[] adj;
 
     private int[] parent;
 
     private boolean[] visited;
 
-    // Constructor
-    public BreadthFirstSearch(int v)
-    {
+
+    public BreadthFirstSearch(int v) {
         V = v;
         adj = new LinkedList[v];
         for (int i = 0; i < v; ++i)
             adj[i] = new LinkedList();
-    }
 
-    // Function to add an edge into the graph
-    public void addEdge(int v, int w) { adj[v].add(w); }
-
-    // prints BFS traversal from a given source s
-    public void BFS(int s)
-    {
-        // Mark all the vertices as not visited(By default
-        // set as false)
         visited = new boolean[V];
         parent = new int[V];
-        Arrays.fill(parent,-1);
 
-        // Create a queue for BFS
+        Arrays.fill(parent, -1);
+    }
+
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+
+    public void BFS(int s) {
+
+
         LinkedList<Integer> queue
                 = new LinkedList<Integer>();
 
-        // Mark the current node as visited and enqueue it
+
         visited[s] = true;
         queue.add(s);
 
         while (queue.size() != 0) {
-            // Dequeue a vertex from queue and print it
             s = queue.poll();
-            System.out.print(s + " ");
 
-            // Get all adjacent vertices of the dequeued
-            // vertex s If a adjacent has not been visited,
-            // then mark it visited and enqueue it
+
             for (int n : adj[s]) {
                 if (!visited[n]) {
                     visited[n] = true;
-                    parent[n]=s;
+                    parent[n] = s;
                     queue.add(n);
                 }
             }
         }
-//        System.out.println();
-//        System.out.println("visited:");
-//        for (boolean b : visited) {
-//            System.out.print(b+" ");
-//        }
-//        System.out.println();
-//        System.out.println("parents:");
-//        for (int i : parent) {
-//            System.out.print(i+" ");
-//        }
-//        System.out.println();
     }
 
     public LinkedList<Integer>[] getAdj() {
--- lab1/src/test/java/BFSTest.java
+++ lab1/src/test/java/BFSTest.java
@@ -2,85 +2,319 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import test.company.lab1.util.BreadthFirstSearch;
-import test.company.lab1.util.Functions;
 
+import java.util.Arrays;
 
 public class BFSTest {
 
     BreadthFirstSearch g;
 
-
     @BeforeAll
-    static void start(){
+    static void start() {
         System.out.println("BFS test's started!");
     }
 
-
     @Nested
-    class TestNestOne{
+    class TestGraphOne {
         @BeforeEach
+        @DisplayName("Первый граф. Малый несвязный")
         public void init() {
-            System.out.println("BFS with first graph");
             g = new BreadthFirstSearch(8);
-            g.addEdge(0, 3);
-            g.addEdge(3, 0);
+            g.addEdge(0, 1);
+            g.addEdge(1, 0);
+            g.addEdge(0, 2);
+            g.addEdge(2, 0);
             g.addEdge(1, 2);
             g.addEdge(2, 1);
-            g.addEdge(1, 5);
-            g.addEdge(5, 1);
-            g.addEdge(1, 6);
-            g.addEdge(6, 1);
-            g.addEdge(2, 4);
-            g.addEdge(4, 2);
             g.addEdge(4, 7);
             g.addEdge(7, 4);
+            g.addEdge(5, 7);
+            g.addEdge(7, 5);
+        }
+
+        @Test
+        @DisplayName("Тест для проверки правильности начальной инициализации списка рёбер, массива предков и массива посещённых вершин")
+        public void firstGraphInitTestMethod() {
+            Assertions.assertArrayEquals(g.getAdj()[0].toArray(), new Integer[]{1, 2}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[1].toArray(), new Integer[]{0, 2}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[2].toArray(), new Integer[]{0, 1}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[3].toArray(), new Integer[]{}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[4].toArray(), new Integer[]{7}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[5].toArray(), new Integer[]{7}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[6].toArray(), new Integer[]{}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[7].toArray(), new Integer[]{4, 5}, "adjacency list is incorrect");
+
+            int[] parentInitial = new int[8];
+            Arrays.fill(parentInitial, -1);
+
+
+            Assertions.assertArrayEquals(g.getParent(), parentInitial, "parents initial state is incorrect");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[8], "visited initial state is incorrect");
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {0})
+        @DisplayName("Тест прохода графа в ширину")
+        public void firstGraphTraverseTestMethod1(int startVertex) {
+
+            System.out.println("Start vertex = " + startVertex);
+            g.BFS(startVertex);
+            Assertions.assertArrayEquals(g.getParent(), new int[]{-1, 0, 0, -1, -1, -1, -1, -1}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{true, true, true, false, false, false, false, false}, "visited don't match with correct visited");
+
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {3})
+        @DisplayName("Тест прохода графа в ширину")
+        public void firstGraphTraverseTestMethod2(int startVertex) {
+
+            System.out.println("Start vertex = " + startVertex);
+            g.BFS(startVertex);
+            Assertions.assertArrayEquals(g.getParent(), new int[]{-1, -1, -1, -1, -1, -1, -1, -1}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{false, false, false, true, false, false, false, false}, "visited don't match with correct visited");
+
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {5})
+        @DisplayName("Тест прохода графа в ширину")
+        public void firstGraphTraverseTestMethod3(int startVertex) {
+
+            System.out.println("Start vertex = " + startVertex);
+            g.BFS(startVertex);
+            Assertions.assertArrayEquals(g.getParent(), new int[]{-1, -1, -1, -1, 7, -1, -1, 5}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{false, false, false, false, true, true, false, true}, "visited don't match with correct visited");
+
+        }
+
+    }
+
+    @Nested
+    class TestGraphTwo {
+        @BeforeEach
+        @DisplayName("Второй граф. Большой связный")
+        public void init() {
+            g = new BreadthFirstSearch(18);
+            g.addEdge(0, 1);
+            g.addEdge(1, 0);
+            g.addEdge(0, 7);
+            g.addEdge(7, 0);
+            g.addEdge(0, 14);
+            g.addEdge(14, 0);
+            g.addEdge(0, 2);
+            g.addEdge(2, 0);
+            g.addEdge(1, 4);
+            g.addEdge(4, 1);
+            g.addEdge(1, 5);
+            g.addEdge(5, 1);
+            g.addEdge(2, 5);
+            g.addEdge(5, 2);
+            g.addEdge(2, 3);
+            g.addEdge(3, 2);
+            g.addEdge(3, 10);
+            g.addEdge(10, 3);
+            g.addEdge(4, 7);
+            g.addEdge(7, 4);
+            g.addEdge(4, 8);
+            g.addEdge(8, 4);
+            g.addEdge(5, 6);
+            g.addEdge(6, 5);
+            g.addEdge(5, 8);
+            g.addEdge(8, 5);
+            g.addEdge(5, 9);
+            g.addEdge(9, 5);
+            g.addEdge(6, 9);
+            g.addEdge(9, 6);
+            g.addEdge(6, 10);
+            g.addEdge(10, 6);
+            g.addEdge(7, 8);
+            g.addEdge(8, 7);
+            g.addEdge(8, 9);
+            g.addEdge(9, 8);
+            g.addEdge(8, 11);
+            g.addEdge(11, 8);
+            g.addEdge(8, 12);
+            g.addEdge(12, 8);
+            g.addEdge(9, 12);
+            g.addEdge(12, 9);
+            g.addEdge(9, 13);
+            g.addEdge(13, 9);
+            g.addEdge(10, 13);
+            g.addEdge(13, 10);
+            g.addEdge(11, 12);
+            g.addEdge(12, 11);
+            g.addEdge(11, 15);
+            g.addEdge(15, 11);
+            g.addEdge(12, 13);
+            g.addEdge(13, 12);
+            g.addEdge(13, 17);
+            g.addEdge(17, 13);
+            g.addEdge(14, 15);
+            g.addEdge(15, 14);
+            g.addEdge(15, 16);
+            g.addEdge(16, 15);
+            g.addEdge(15, 17);
+            g.addEdge(17, 15);
+            g.addEdge(16, 17);
+            g.addEdge(17, 16);
+
+        }
+
+
+        @Test
+        @DisplayName("Тест для проверки правильности начальной инициализации списка рёбер, массива предков и массива посещённых вершин")
+        public void secondGraphInitTestMethod() {
+
+            Assertions.assertArrayEquals(g.getAdj()[0].toArray(), new Integer[]{1, 7, 14, 2}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[1].toArray(), new Integer[]{0, 4, 5}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[2].toArray(), new Integer[]{0, 5, 3}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[3].toArray(), new Integer[]{2, 10}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[4].toArray(), new Integer[]{1, 7, 8}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[5].toArray(), new Integer[]{1, 2, 6, 8, 9}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[6].toArray(), new Integer[]{5, 9, 10}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[7].toArray(), new Integer[]{0, 4, 8}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[8].toArray(), new Integer[]{4, 5, 7, 9, 11, 12}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[9].toArray(), new Integer[]{5, 6, 8, 12, 13}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[10].toArray(), new Integer[]{3, 6, 13}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[11].toArray(), new Integer[]{8, 12, 15}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[12].toArray(), new Integer[]{8, 9, 11, 13}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[13].toArray(), new Integer[]{9, 10, 12, 17}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[14].toArray(), new Integer[]{0, 15}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[15].toArray(), new Integer[]{11, 14, 16, 17}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[16].toArray(), new Integer[]{15, 17}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[17].toArray(), new Integer[]{13, 15, 16}, "adjacency list is incorrect");
+
+
+            int[] parentInitial = new int[18];
+            Arrays.fill(parentInitial, -1);
+
+
+            Assertions.assertArrayEquals(g.getParent(), parentInitial, "parents initial state is incorrect");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[18], "visited initial state is incorrect");
         }
 
         @ParameterizedTest
         @ValueSource(ints = {2})
-        public void firstTestMethod1(int startVertex) {
-
-            System.out.println("Start vertex = "+startVertex);
+        @DisplayName("Тест прохода графа в ширину")
+        public void secondGraphTraverseTestMethod1(int startVertex) {
+            System.out.println("Start vertex = " + startVertex);
             g.BFS(startVertex);
-            Assertions.assertArrayEquals(g.getParent(),new int[]{-1, 2, -1, -1, 2, 1, 1, 4},"parents don't match with correct parents");
-            Assertions.assertArrayEquals(g.getVisited(),new boolean[]{false, true, true, false, true, true, true, true},"visited don't match with correct visited");
+            Assertions.assertArrayEquals(g.getParent(), new int[]{2, 0, -1, 2, 1, 2, 5, 0, 5, 5, 3, 8, 8, 9, 0, 14, 15, 15}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true}, "visited don't match with correct visited");
 
         }
 
-        @Test
-        public void firstTestMethod2(int startVertex) {
-            System.out.println("This is method 2 in first graph");
+
+        @ParameterizedTest
+        @ValueSource(ints = {7})
+        @DisplayName("Тест прохода графа в ширину")
+        public void secondGraphTraverseTestMethod2(int startVertex) {
+            System.out.println("Start vertex = " + startVertex);
+            g.BFS(startVertex);
+            Assertions.assertArrayEquals(g.getParent(), new int[]{7, 0, 0, 2, 7, 8, 5, -1, 7, 8, 3, 8, 8, 9, 0, 14, 15, 15}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true}, "visited don't match with correct visited");
+        }
+
+
+        @ParameterizedTest
+        @ValueSource(ints = {17})
+        @DisplayName("Тест прохода графа в ширину")
+        public void secondGraphTraverseTestMethod3(int startVertex) {
+            System.out.println("Start vertex = " + startVertex);
+            g.BFS(startVertex);
+            Assertions.assertArrayEquals(g.getParent(), new int[]{14, 5, 5, 10, 8, 9, 9, 8, 9, 13, 13, 15, 13, 17, 15, 17, 17, -1}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true}, "visited don't match with correct visited");
         }
     }
+
 
     @Nested
-    class TestNestTwo{
+    class TestGraphThree {
         @BeforeEach
+        @DisplayName("Второй граф. Малый связный")
         public void init() {
-            System.out.println("BFS with second graph");
             g = new BreadthFirstSearch(8);
+            g.addEdge(0, 1);
+            g.addEdge(1, 0);
             g.addEdge(0, 3);
             g.addEdge(3, 0);
-            g.addEdge(1, 2);
-            g.addEdge(2, 1);
+            g.addEdge(0, 4);
+            g.addEdge(4, 0);
             g.addEdge(1, 5);
             g.addEdge(5, 1);
-            g.addEdge(2, 6);
-            g.addEdge(6, 2);
-            g.addEdge(5, 6);
-            g.addEdge(6, 5);
-            g.addEdge(4, 7);
-            g.addEdge(7, 4);
+            g.addEdge(2, 4);
+            g.addEdge(4, 2);
+            g.addEdge(2, 5);
+            g.addEdge(5, 2);
+            g.addEdge(3, 5);
+            g.addEdge(5, 3);
+            g.addEdge(3, 7);
+            g.addEdge(7, 3);
+            g.addEdge(4, 6);
+            g.addEdge(6, 4);
         }
 
         @Test
-        public void secondTestMethod1(int startVertex) {
-            System.out.println("This is method 1 in second graph");
+        @DisplayName("Тест для проверки правильности начальной инициализации списка рёбер, массива предков и массива посещённых вершин")
+        public void thirdGraphInitTestMethod() {
+
+            Assertions.assertArrayEquals(g.getAdj()[0].toArray(), new Integer[]{1, 3, 4}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[1].toArray(), new Integer[]{0, 5}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[2].toArray(), new Integer[]{4, 5}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[3].toArray(), new Integer[]{0, 5, 7}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[4].toArray(), new Integer[]{0, 2, 6}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[5].toArray(), new Integer[]{1, 2, 3}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[6].toArray(), new Integer[]{4}, "adjacency list is incorrect");
+            Assertions.assertArrayEquals(g.getAdj()[7].toArray(), new Integer[]{3}, "adjacency list is incorrect");
+
+
+            int[] parentInitial = new int[8];
+            Arrays.fill(parentInitial, -1);
+
+
+            Assertions.assertArrayEquals(g.getParent(), parentInitial, "parents initial state is incorrect");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[8], "visited initial state is incorrect");
         }
 
-        @Test
-        public void secondTestMethod2(int startVertex) {
-            System.out.println("This is method 2 in second graph");
+        @ParameterizedTest
+        @ValueSource(ints = {1})
+        @DisplayName("Тест прохода графа в ширину")
+        public void thirdGraphTraverseTestMethod1(int startVertex) {
+
+            System.out.println("Start vertex = " + startVertex);
+            g.BFS(startVertex);
+            Assertions.assertArrayEquals(g.getParent(), new int[]{1, -1, 5, 0, 0, 1, 4, 3}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{true, true, true, true, true, true, true, true}, "visited don't match with correct visited");
+
         }
+
+        @ParameterizedTest
+        @ValueSource(ints = {4})
+        @DisplayName("Тест прохода графа в ширину")
+        public void thirdGraphTraverseTestMethod2(int startVertex) {
+
+            System.out.println("Start vertex = " + startVertex);
+            g.BFS(startVertex);
+            Assertions.assertArrayEquals(g.getParent(), new int[]{4, 0, 4, 0, -1, 2, 4, 3}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{true, true, true, true, true, true, true, true}, "visited don't match with correct visited");
+
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {7})
+        @DisplayName("Тест прохода графа в ширину")
+        public void thirdGraphTraverseTestMethod3(int startVertex) {
+
+            System.out.println("Start vertex = " + startVertex);
+            g.BFS(startVertex);
+            Assertions.assertArrayEquals(g.getParent(), new int[]{3, 0, 5, 7, 0, 3, 4, -1}, "parents don't match with correct parents");
+            Assertions.assertArrayEquals(g.getVisited(), new boolean[]{true, true, true, true, true, true, true, true}, "visited don't match with correct visited");
+
+        }
+
+
     }
+
+
 }
