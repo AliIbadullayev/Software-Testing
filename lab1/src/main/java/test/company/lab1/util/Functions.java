92a4e4bc85a45668d3c77d66c11af7ead8344d0a
Ibadullaev_Ali <ali.ibadullayev.02@gmail.com>
2023-03-04 13:05:18 +0300

    add test for first task

--- lab1/src/main/java/test/company/lab1/util/Functions.java
+++ lab1/src/main/java/test/company/lab1/util/Functions.java
@@ -5,29 +5,32 @@ import java.math.BigInteger;
 import java.math.MathContext;
 
 public class Functions {
+    private static final int DIGITS_AFTER_COMMA = 3;
+
     public static double funSecInPowerSeries(double x){
         BigDecimal res = BigDecimal.ZERO;
         if (Math.abs(x) == Math.PI/2) return Double.NaN;
         for (int i = 0; i < 60; i++){
             res = res.add(BigDecimal.valueOf(Math.pow(-1, i)*Math.pow(x, 2*i)).divide(new BigDecimal(factorialUsingForLoop(2*i)), MathContext.DECIMAL128));
         }
-        return (double) Math.round(1/res.doubleValue()*1000)/1000;
-
+        return formatOutput(1/res.doubleValue());
     }
+
     public static BigInteger factorialUsingForLoop(int n) {
         BigInteger factorial = BigInteger.ONE;
-
 
         for (int i = 1; i <= n; i++) {
             factorial = factorial.multiply(BigInteger.valueOf(i));
         }
-
-
         return factorial;
     }
 
     public static double funSec(double x){
         if (Math.abs(x) == Math.PI/2) return Double.NaN;
-        return  (double) Math.round(1/Math.cos(x)*1000)/1000 ;
+        return formatOutput(1/Math.cos(x)) ;
+    }
+
+    private static double formatOutput(double value){
+        return (double) Math.round(value*Math.pow(10, DIGITS_AFTER_COMMA))/Math.pow(10, DIGITS_AFTER_COMMA);
     }
 }
--- lab1/src/test/java/FunctionTest.java
+++ lab1/src/test/java/FunctionTest.java
@@ -10,27 +10,34 @@ import java.util.List;
 
 public class FunctionTest {
 
-    static List<Integer> list;
     @BeforeAll
     static void start(){
         System.out.println("Test's started!");
-        list = new ArrayList<Integer>();
-        list.add(1);
     }
 
-    @ParameterizedTest
-    @ValueSource(doubles = {Math.PI/4, Math.PI/6, Math.PI/3})
-    @DisplayName("Тест для проверки равности результата sec(x) и степенного ряда (-Pi/2;Pi/2)")
-    void checkSecFunc1(double x){
-        System.out.println("х = " + x);
-        System.out.print("sec(x) = " + Functions.funSec(x)+"; " + "степенной ряд = "+Functions.funSecInPowerSeries(x)+"\n\n");
-        Assertions.assertTrue(Math.abs(Functions.funSec(x) - Functions.funSecInPowerSeries(x)) < 0.001d, "test not passed!");
-        System.out.println();
-    }
+    /*
+    * 1) Значения, при которых функция sec(x) имеет разрыв или не определена (например, x=0, x=π/2, x=-π/2 и т.д.).
+    * Эти значения могут быть важными для проверки корректности обработки исключительных ситуаций в коде.
+      2) Значения, при которых функция sec(x) принимает очень большие или очень маленькие значения (например, x=π/2+0.001, x=π/2-0.001, x=0.001, x=-0.001 и т.д.).
+      * Эти значения могут помочь обнаружить ошибки в вычислениях из-за потери точности при работе с большими или маленькими числами.
+      3) Значения, при которых функция sec(x) принимает нормальные значения (например, x=π/6, x=π/3, x=π/4 и т.д.).
+      * Эти значения могут быть важными для проверки соответствия реализации функции ее математическому определению и обнаружения ошибок в округлении или обработке аргументов функции.
+
+    * */
+
 
     @ParameterizedTest
     @ValueSource(doubles = {Math.PI/2, -Math.PI/2,  Math.PI, -Math.PI})
-    @DisplayName("Тест для проверки равности результата sec(x) и степенного ряда  в граничных точках Pi/2, -Pi/2 и Pi, -Pi!")
+    @DisplayName("Тест для проверки равности результата sec(x) и степенного ряда  в точках разрыва и в координатах пересекающих ось X!")
+    void checkSecFunc1(double x){
+        System.out.println("х = " + x);
+        System.out.print("sec(x) = " + Functions.funSec(x)+"; " + "степенной ряд = "+Functions.funSecInPowerSeries(x)+"\n\n");
+        Assertions.assertEquals(Functions.funSec(x), Functions.funSecInPowerSeries(x), "test not passed!");
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {Math.PI/2 - 0.001 , Math.PI/2 + 0.001, 0.001, -0.001, Math.PI+0.001, -Math.PI + 0.001})
+    @DisplayName("Тест для проверки равности результата sec(x) и степенного ряда в точках, где значение функции максимально/минимально!")
     void checkSecFunc2(double x){
         System.out.println("х = " + x);
         System.out.print("sec(x) = " + Functions.funSec(x)+"; " + "степенной ряд = "+Functions.funSecInPowerSeries(x)+"\n\n");
@@ -38,14 +45,12 @@ public class FunctionTest {
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {Math.PI - Math.PI/6, 2*Math.PI/3})
-    @DisplayName("Тест для проверки равности результата sec(x) и степенного ряда  во второй четверти!")
+    @ValueSource(doubles = {Math.PI/4, Math.PI/6, Math.PI/3, 3*Math.PI/4, -Math.PI/3, -2*Math.PI/3})
+    @DisplayName("Тест для проверки равности результата sec(x) и степенного ряда со значениями близкими к целочисленным кратным π")
     void checkSecFunc3(double x){
         System.out.println("х = " + x);
         System.out.print("sec(x) = " + Functions.funSec(x)+"; " + "степенной ряд = "+Functions.funSecInPowerSeries(x)+"\n\n");
-        Assertions.assertEquals(Functions.funSec(x), Functions.funSecInPowerSeries(x), "test not passed!");
+        Assertions.assertTrue(Math.abs(Functions.funSec(x) - Functions.funSecInPowerSeries(x)) < 0.001d, "test not passed!");
+        System.out.println();
     }
-
-
-
 }
